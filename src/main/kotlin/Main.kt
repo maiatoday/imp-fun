9cf1023bd19a0fbe1298fafdc323b14300f29945
Maia Grotepass <maiatoday@gmail.com>
2023-06-10 16:54:44 +0200

    Assisted Functional

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,48 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-
     val crtW = 40
     val crtH = 6
 
     data class Instruction(val ticks: Int = 1, val inc: Int = 0)
 
-    fun crtDisplay(input: List<String>) {
-        val instructions: List<Instruction> = buildList {
-            // transform a list of Strings to a list of Instructions
-            for (s in input) {
-                // transform String to Instruction
-                val opcode = s.substringBefore(" ")
-                val instruction = when (opcode) {
-                    "noop" -> Instruction()
-                    "addx" -> Instruction(ticks = 2, inc = s.substringAfter(" ").toInt())
-                    else -> error("oops")
-                }
-                this.add(instruction)
+    fun parseInstruction(s: String): Instruction {
+        val opcode = s.substringBefore(" ")
+        return when (opcode) {
+            "noop" -> Instruction()
+            "addx" -> Instruction(ticks = 2, inc = s.substringAfter(" ").toInt())
+            else -> error("oops")
+        }
+    }
+
+    fun buildXRegisterAtTick(instructions: List<Instruction>): List<Int> {
+        var x = 1 // needs a running x register value
+        return instructions.flatMap { instruction ->
+            List(instruction.ticks) { x }.apply { x += instruction.inc }
+        } + x
+    }
+
+    fun printCRT(input: List<String>) {
+        val instructions = input.map(::parseInstruction)
+        val xRegisterAtTick = buildXRegisterAtTick(instructions)
+
+        val crtRows = (crtW * crtH until 0 step -crtW).reversed().map { lineStart ->
+            (lineStart until lineStart + crtW).joinToString("") { i ->
+                if (i % crtW in xRegisterAtTick[i] - 1..xRegisterAtTick[i] + 1) "█" else "."
             }
         }
-        val xRegisterAtTick: List<Int> = buildList {
-            // transform instructions to x register values
-            var x = 1 // needs a running x register value
-            for (i in instructions) {
-                repeat(i.ticks) {
-                    this.add(x)
-                }
-                x += i.inc
-            }
-            this.add(x)
-        }
-        for (i in 0 until (crtW * crtH)) {
-            // transform xRegister values to pixels(String)
-            // chop up in lines
-            // side effect: print pixels
-            if (i.mod(crtW) == 0) print("\n")                                                      //<============  side effect
-            print(if (i.mod(crtW) in xRegisterAtTick[i] - 1..xRegisterAtTick[i] + 1) "█" else ".") //<============  side effect
-        }
+
+        println(crtRows.joinToString("\n"))
     }
 
     val testInput = readInput("Day_test")
     println(" ============== test input =============")
-    crtDisplay(testInput)
+    printCRT(testInput)
     println("\n\n")
     println("============== real input ==============")
     val input = readInput("Day")
-    crtDisplay(input)
+    printCRT(input)
     println("\n\n")
-
 }
 
 fun readInput(name: String) = File("src/main/resources/", "$name.txt").readLines()
